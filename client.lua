06a253a44204ada1cee83a33275883a0f2a72e37
feelfreetofee <89666307+feelfreetofee@users.noreply.github.com>
2022-07-10 10:39:16 +0200

    Update client.lua
    https://gta-objects.xyz/objects/
    https://wiki.rage.mp/index.php?title=Weapons

--- client.lua
+++ client.lua
@@ -1,41 +1,41 @@
 local weaponData = { -- Thanks to https://github.com/itzhapp/ShowWeapon
 	back = {
 		list = {
-			['WEAPON_ADVANCEDRIFLE']		= {model = 'w_ar_advancedrifle'		},
-			['WEAPON_ASSAULTRIFLE']			= {model = 'w_ar_assaultrifle'		},
-			['WEAPON_ASSAULTSHOTGUN']		= {model = 'w_sg_assaultshotgun'	},
-			['WEAPON_ASSAULTSMG']			= {model = 'w_sb_assaultsmg'		},
-			['WEAPON_BULLPUPRIFLE_MK2']		= {model = 'w_ar_bullpupriflemk2'	},
-			['WEAPON_BULLPUPRIFLE']			= {model = 'w_ar_bullpuprifle'		},
-			['WEAPON_BULLPUPSHOTGUN']		= {model = 'w_sg_bullpupshotgun'	},
-			['WEAPON_CARBINERIFLE_MK2']		= {model = 'w_ar_carbineriflemk2'	},
-			['WEAPON_CARBINERIFLE']			= {model = 'w_ar_carbinerifle'		},
-			['WEAPON_COMBATPDW'] 			= {model = 'w_sb_smg'			},
-			['WEAPON_COMBATSHOTGUN']		= {model = 'w_sg_pumpshotgun'		},
-			['WEAPON_COMPACTRIFLE']			= {model = 'w_ar_assaultrifle'		},
-			['WEAPON_DBSHOTGUN']			= {model = 'w_sg_sawnoff'		},
-			['WEAPON_FIREWORK']			= {model = 'w_lr_firework'		},
-			['WEAPON_GUSENBERG']			= {model = 'w_sb_gusenberg'		},
-			['WEAPON_HEAVYSHOTGUN']			= {model = 'w_sg_heavyshotgun'		},
-			['WEAPON_HEAVYSNIPER_MK2']		= {model = 'w_sr_heavysnipermk2'	},
-			['WEAPON_HEAVYSNIPER']			= {model = 'w_sr_heavysniper'		},
-			['WEAPON_MARKSMANRIFLE']		= {model = 'w_sr_marksmanrifle'		},
-			['WEAPON_MARKSMANRIFLE_MK2']		= {model = 'w_sr_marksmanriflemk2'	},
-			['WEAPON_MG']				= {model = 'w_mg_mg'			},
-			['WEAPON_MILITARYRIFLE']		= {model = 'w_sr_marksmanriflemk2'	},
-			['WEAPON_MUSKET']			= {model = 'w_ar_musket'		},
-			['WEAPON_MICROSMG'] 			= {model = 'w_sb_microsmg'		},
-			['WEAPON_PUMPSHOTGUN_MK2']		= {model = 'w_sg_pumpshotgunmk2'	},
-			['WEAPON_PUMPSHOTGUN']			= {model = 'w_sg_pumpshotgun'		},
-			['WEAPON_SAWNOFFSHOTGUN']		= {model = 'w_sg_sawnoff'		},
-			['WEAPON_SMG_MK2']			= {model = 'w_sb_smgmk2'		},
-			['WEAPON_SMG']				= {model = 'w_sb_smg'			},
-			['WEAPON_SNIPERRIFLE']			= {model = 'w_sr_sniperrifle'		},
-			['WEAPON_SPECIALCARBINE_MK2']		= {model = 'w_ar_specialcarbinemk2'	},
-			['WEAPON_SPECIALCARBINE']     		= {model = 'w_ar_specialcarbine'	},
-			['WEAPON_SWEEPERSHOTGUN']    		= {model = 'w_sg_sweeper'		},
-			['WEAPON_COMBATMG']     	 	= {model = 'w_mg_combatmg'		},
-			['WEAPON_COMBATMG_MK2']       		= {model = 'w_mg_combatmgmk2'		},
+			['WEAPON_ADVANCEDRIFLE']		= {model = 'w_ar_advancedrifle', 	pos = vector3(0.0, 0.0, 0.0), 	rot = vector3(0.0, 0.0, 0.0)},
+			['WEAPON_ASSAULTRIFLE']			= {model = 'w_ar_assaultrifle', 	pos = vector3(0.0, 0.0, 0.0), 	rot = vector3(0.0, 0.0, 0.0)},
+			['WEAPON_ASSAULTSHOTGUN']		= {model = 'w_sg_assaultshotgun', 	pos = vector3(0.0, 0.0, 0.0), 	rot = vector3(0.0, 0.0, 0.0)},
+			['WEAPON_ASSAULTSMG']			= {model = 'w_sb_assaultsmg', 		pos = vector3(0.0, 0.0, 0.0), 	rot = vector3(0.0, 0.0, 0.0)},
+			['WEAPON_BULLPUPRIFLE_MK2']		= {model = 'w_ar_bullpupriflemk2', 	pos = vector3(0.0, 0.0, 0.0), 	rot = vector3(0.0, 0.0, 0.0)},
+			['WEAPON_BULLPUPRIFLE']			= {model = 'w_ar_bullpuprifle', 	pos = vector3(0.0, 0.0, 0.0), 	rot = vector3(0.0, 0.0, 0.0)},
+			['WEAPON_BULLPUPSHOTGUN']		= {model = 'w_sg_bullpupshotgun', 	pos = vector3(0.0, 0.0, 0.0), 	rot = vector3(0.0, 0.0, 0.0)},
+			['WEAPON_CARBINERIFLE_MK2']		= {model = 'w_ar_carbineriflemk2', 	pos = vector3(0.0, 0.0, 0.0), 	rot = vector3(0.0, 0.0, 0.0)},
+			['WEAPON_CARBINERIFLE']			= {model = 'w_ar_carbinerifle', 	pos = vector3(0.0, 0.0, 0.0), 	rot = vector3(0.0, 0.0, 0.0)},
+			['WEAPON_COMBATPDW'] 			= {model = 'w_sb_smg', 				pos = vector3(0.0, 0.0, 0.0), 	rot = vector3(0.0, 0.0, 0.0)},
+			['WEAPON_COMBATSHOTGUN']		= {model = 'w_sg_pumpshotgun', 		pos = vector3(0.0, 0.0, 0.0), 	rot = vector3(0.0, 0.0, 0.0)},
+			['WEAPON_COMPACTRIFLE']			= {model = 'w_ar_assaultrifle', 	pos = vector3(0.0, 0.0, 0.0), 	rot = vector3(0.0, 0.0, 0.0)},
+			['WEAPON_DBSHOTGUN']			= {model = 'w_sg_sawnoff', 			pos = vector3(0.0, 0.0, 0.0), 	rot = vector3(0.0, 0.0, 0.0)},
+			['WEAPON_FIREWORK']				= {model = 'w_lr_firework', 		pos = vector3(0.0, 0.0, 0.0), 	rot = vector3(0.0, 0.0, 0.0)},
+			['WEAPON_GUSENBERG']			= {model = 'w_sb_gusenberg', 		pos = vector3(0.0, 0.0, 0.0), 	rot = vector3(0.0, 0.0, 0.0)},
+			['WEAPON_HEAVYSHOTGUN']			= {model = 'w_sg_heavyshotgun', 	pos = vector3(0.0, 0.0, 0.0), 	rot = vector3(0.0, 0.0, 0.0)},
+			['WEAPON_HEAVYSNIPER_MK2']		= {model = 'w_sr_heavysnipermk2', 	pos = vector3(0.0, 0.0, 0.0), 	rot = vector3(0.0, 0.0, 0.0)},
+			['WEAPON_HEAVYSNIPER']			= {model = 'w_sr_heavysniper', 		pos = vector3(0.0, 0.0, 0.0), 	rot = vector3(0.0, 0.0, 0.0)},
+			['WEAPON_MARKSMANRIFLE']		= {model = 'w_sr_marksmanrifle', 	pos = vector3(0.0, 0.0, 0.0), 	rot = vector3(0.0, 0.0, 0.0)},
+			['WEAPON_MARKSMANRIFLE_MK2']	= {model = 'w_sr_marksmanriflemk2', pos = vector3(0.0, 0.0, 0.0), 	rot = vector3(0.0, 0.0, 0.0)},
+			['WEAPON_MG']					= {model = 'w_mg_mg', 				pos = vector3(0.0, 0.0, 0.0), 	rot = vector3(0.0, 0.0, 0.0)},
+			['WEAPON_MILITARYRIFLE']		= {model = 'w_sr_marksmanriflemk2', pos = vector3(0.0, 0.0, 0.0), 	rot = vector3(0.0, 0.0, 0.0)},
+			['WEAPON_MUSKET']				= {model = 'w_ar_musket', 			pos = vector3(0.0, 0.0, 0.0), 	rot = vector3(0.0, 0.0, 0.0)},
+			['WEAPON_MICROSMG'] 			= {model = 'w_sb_microsmg', 		pos = vector3(0.0, 0.0, 0.0), 	rot = vector3(0.0, 0.0, 0.0)},
+			['WEAPON_PUMPSHOTGUN_MK2']		= {model = 'w_sg_pumpshotgunmk2', 	pos = vector3(0.0, 0.0, 0.0), 	rot = vector3(0.0, 0.0, 0.0)},
+			['WEAPON_PUMPSHOTGUN']			= {model = 'w_sg_pumpshotgun', 		pos = vector3(0.0, 0.0, 0.0), 	rot = vector3(0.0, 0.0, 0.0)},
+			['WEAPON_SAWNOFFSHOTGUN']		= {model = 'w_sg_sawnoff', 			pos = vector3(0.0, 0.0, 0.0), 	rot = vector3(0.0, 0.0, 0.0)},
+			['WEAPON_SMG_MK2']				= {model = 'w_sb_smgmk2', 			pos = vector3(0.0, 0.0, 0.0), 	rot = vector3(0.0, 0.0, 0.0)},
+			['WEAPON_SMG']					= {model = 'w_sb_smg', 				pos = vector3(0.0, 0.0, 0.0), 	rot = vector3(0.0, 0.0, 0.0)},
+			['WEAPON_SNIPERRIFLE']			= {model = 'w_sr_sniperrifle', 		pos = vector3(0.0, 0.0, 0.0), 	rot = vector3(0.0, 0.0, 0.0)},
+			['WEAPON_SPECIALCARBINE_MK2']	= {model = 'w_ar_specialcarbinemk2',pos = vector3(0.0, 0.0, 0.0),	rot = vector3(0.0, 0.0, 0.0)},
+			['WEAPON_SPECIALCARBINE']     	= {model = 'w_ar_specialcarbine', 	pos = vector3(0.0, 0.0, 0.0), 	rot = vector3(0.0, 0.0, 0.0)},
+			['WEAPON_SWEEPERSHOTGUN']    	= {model = 'w_sg_sweeper', 			pos = vector3(0.0, 0.0, 0.0), 	rot = vector3(0.0, 0.0, 0.0)},
+			['WEAPON_COMBATMG']     	 	= {model = 'w_mg_combatmg', 		pos = vector3(0.0, 0.0, 0.0), 	rot = vector3(0.0, 0.0, 0.0)},
+			['WEAPON_COMBATMG_MK2']       	= {model = 'w_mg_combatmgmk2', 		pos = vector3(0.0, 0.0, 0.0), 	rot = vector3(0.0, 0.0, 0.0)},
 		},
 		coords = {
 			pos = vector3(0.3, -0.15, 0.05),
@@ -44,7 +44,7 @@ local weaponData = { -- Thanks to https://github.com/itzhapp/ShowWeapon
 	},
 	front = {
 		list = {
-			['WEAPON_ASSAULTRIFLE_MK2']		= {model = 'w_ar_assaultriflemk2'	},
+			['WEAPON_ASSAULTRIFLE_MK2']		= {model = 'w_ar_assaultriflemk2', 	pos = vector3(0.0, 0.0, 0.0), 	rot = vector3(0.0, 0.0, 0.0)},
 		},
 		coords = {
 			pos = vector3(0.2, 0.20, -0.1),
@@ -53,9 +53,18 @@ local weaponData = { -- Thanks to https://github.com/itzhapp/ShowWeapon
 	},
 	left = {
 		list = {
-			['WEAPON_NIGHTSTICK']		= {model = 'w_me_nightstick'	},
-			['WEAPON_MACHETE']		= {model = 'prop_ld_w_me_machette'	},
-			-- ['WEAPON_CROWBAR']		= {model = 'w_me_crowbar'	},
+			['WEAPON_DAGGER']				= {model = 'w_me_dagger', 			pos = vector3(0.0, 0.0, 0.0), 	rot = vector3(0.0, 0.0, 0.0)},
+			['WEAPON_BOTTLE']				= {model = 'w_me_bottle', 			pos = vector3(0.0, 0.0, 0.0), 	rot = vector3(0.0, 0.0, 0.0)},
+			['WEAPON_CROWBAR']				= {model = 'w_me_crowbar', 			pos = vector3(0.0, 0.0, 0.0), 	rot = vector3(0.0, 0.0, 0.0)},
+			['WEAPON_HAMMER']				= {model = 'w_me_hammer', 			pos = vector3(0.0, 0.0, 0.0), 	rot = vector3(0.0, 0.0, 0.0)},
+			['WEAPON_HATHCET']				= {model = 'w_me_hatchet', 			pos = vector3(0.0, 0.0, 0.0), 	rot = vector3(0.0, 0.0, 0.0)},
+			['WEAPON_KNIFE']				= {model = 'w_me_knife_01', 		pos = vector3(0.0, 0.0, 0.0), 	rot = vector3(0.0, 0.0, 0.0)},
+			['WEAPON_MACHETE']				= {model = 'prop_ld_w_me_machette', pos = vector3(0.0, 0.0, 0.0), 	rot = vector3(0.0, 0.0, 0.0)},
+			['WEAPON_SWITCHBLADE']			= {model = 'prop_w_me_knife_01',	pos = vector3(0.0, 0.0, 0.0), 	rot = vector3(0.0, 0.0, 0.0)},
+			['WEAPON_NIGHTSTICK']			= {model = 'w_me_nightstick', 		pos = vector3(0.0, 0.0, 0.0), 	rot = vector3(0.0, 0.0, 0.0)},
+			['WEAPON_WRENCH']				= {model = 'prop_tool_wrench', 		pos = vector3(0.0, 0.0, 0.0), 	rot = vector3(0.0, 0.0, 0.0)},
+			['WEAPON_BATTLEAXE']			= {model = 'prop_tool_fireaxe', 	pos = vector3(0.0, 0.0, 0.0), 	rot = vector3(0.0, 0.0, 0.0)},
+			['WEAPON_STONE_HATCHET']		= {model = 'w_me_hatchet', 			pos = vector3(0.0, 0.0, 0.0), 	rot = vector3(0.0, 0.0, 0.0)},
 		},
 		coords = {
 			pos = vector3(0.0, 0.0, 0.225),
@@ -71,20 +80,20 @@ local weaponHolstered = {
 }
 local weaponEquipped = {name = nil, object = nil}
 
-AddEventHandler('playerDropped', function() -- onResourceStop
+AddEventHandler('onResourceStop', function() --playerDropped
 	for k, v in pairs(GetGamePool('CObject')) do
-		if IsEntityAttachedToEntity(PlayerPedId(), v) then
-		    SetEntityAsMissionEntity(v, true, true)
-		    DeleteObject(v)
-		    DeleteEntity(v)
-		end
+        if IsEntityAttachedToEntity(PlayerPedId(), v) then
+            SetEntityAsMissionEntity(v, true, true)
+            DeleteObject(v)
+            DeleteEntity(v)
+        end
 	end
 end)
 
 AddEventHandler('ox_inventory:currentWeapon',function(currentWeapon)
-	local playerPed = PlayerPedId()
-	if not currentWeapon then 
-		if weaponEquipped and weaponEquipped.name then
+    local playerPed = PlayerPedId()
+    if not currentWeapon then 
+        if weaponEquipped and weaponEquipped.name then
 			for k in pairs(weaponData) do
 				if weaponData[k].list[weaponEquipped and weaponEquipped.name] then
 					local model = weaponData[k].list[weaponEquipped.name].model
@@ -98,23 +107,24 @@ AddEventHandler('ox_inventory:currentWeapon',function(currentWeapon)
 
 					weaponHolstered[k].name = weaponEquipped.name
 					weaponHolstered[k].object = CreateObject(model, 0, 0, 0, true, true, true)
-
-					AttachEntityToEntity(weaponHolstered[k].object, playerPed, GetPedBoneIndex(playerPed, 24816), weaponData[k].coords.pos, weaponData[k].coords.rot, true, true, false, false, 1, true)
+					
+					local pos = weaponData[k].coords.pos - weaponData[k].list[weaponEquipped.name].pos
+					local rot = weaponData[k].coords.rot - weaponData[k].list[weaponEquipped.name].rot
+					
+					AttachEntityToEntity(weaponHolstered[k].object, playerPed, GetPedBoneIndex(playerPed, 24816), pos, rot, true, true, false, false, 1, true)
 					
 					SetModelAsNoLongerNeeded(model)
 				end
 			end
-		end
-	else
-		weaponEquipped.name = currentWeapon.name
-		if currentWeapon then
-			for k in pairs(weaponData) do
-				if currentWeapon.name == weaponHolstered[k].name then
-					DeleteObject(weaponHolstered[k].object)
-					weaponHolstered[k].name = nil
-					weaponHolstered[k].object = nil
-				end
+        end
+    else
+        weaponEquipped.name = currentWeapon.name
+		for k in pairs(weaponData) do
+			if currentWeapon.name == weaponHolstered[k].name then
+				DeleteObject(weaponHolstered[k].object)
+				weaponHolstered[k].name = nil
+				weaponHolstered[k].object = nil
 			end
 		end
-	end
+    end
 end)
