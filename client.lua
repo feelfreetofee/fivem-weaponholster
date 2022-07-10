edb54be41d003b0a70152a8d83b4031c539aac6d
feelfreetofee <89666307+feelfreetofee@users.noreply.github.com>
2022-07-10 09:51:09 +0200

    just a little prerelease

--- client.lua
+++ client.lua
@@ -1,114 +1,120 @@
-local weaponData =
-{ -- Thanks to https://github.com/itzhapp/ShowWeapon
-    ['WEAPON_ADVANCEDRIFLE']      = {model = 'w_ar_advancedrifle',      onBack = true},
-    -- ['WEAPON_ASSAULTRIFLE_MK2']   = {model = 'w_ar_assaultriflemk2',    onBack = true},
-    ['WEAPON_ASSAULTRIFLE']       = {model = 'w_ar_assaultrifle',       onBack = true},
-    ['WEAPON_ASSAULTSHOTGUN']     = {model = 'w_sg_assaultshotgun',     onBack = true},
-    ['WEAPON_ASSAULTSMG']         = {model = 'w_sb_assaultsmg',         onBack = true},
-    ['WEAPON_BULLPUPRIFLE_MK2']   = {model = 'w_ar_bullpupriflemk2',    onBack = true},
-    ['WEAPON_BULLPUPRIFLE']       = {model = 'w_ar_bullpuprifle',       onBack = true},
-    ['WEAPON_BULLPUPSHOTGUN']     = {model = 'w_sg_bullpupshotgun',     onBack = true},
-    ['WEAPON_CARBINERIFLE_MK2']   = {model = 'w_ar_carbineriflemk2',    onBack = true},
-    ['WEAPON_CARBINERIFLE']       = {model = 'w_ar_carbinerifle',     	onBack = true},
-    ['WEAPON_COMBATPDW']          = {model = 'w_sb_smg',                onBack = true},
-    ['WEAPON_COMBATSHOTGUN']      = {model = 'w_sg_pumpshotgun',        onBack = true},
-    ['WEAPON_COMPACTRIFLE']       = {model = 'w_ar_assaultrifle',       onBack = true},
-    ['WEAPON_DBSHOTGUN']          = {model = 'w_sg_sawnoff',            onBack = true},
-    ['WEAPON_FIREWORK']           = {model = 'w_lr_firework',           onBack = true},
-    ['WEAPON_GUSENBERG']          = {model = 'w_sb_gusenberg',          onBack = true},
-    ['WEAPON_HEAVYSHOTGUN']       = {model = 'w_sg_heavyshotgun',       onBack = true},
-    ['WEAPON_HEAVYSNIPER_MK2']    = {model = 'w_sr_heavysnipermk2',     onBack = true},
-    ['WEAPON_HEAVYSNIPER']        = {model = 'w_sr_heavysniper',        onBack = true},
-    ['WEAPON_MARKSMANRIFLE']      = {model = 'w_sr_marksmanrifle',      onBack = true},
-    ['WEAPON_MARKSMANRIFLE_MK2']  = {model = 'w_sr_marksmanriflemk2',   onBack = true},
-    ['WEAPON_MG']                 = {model = 'w_mg_mg',                 onBack = true},
-    ['WEAPON_MILITARYRIFLE']      = {model = 'w_sr_marksmanriflemk2',   onBack = true},
-    ['WEAPON_MUSKET']             = {model = 'w_ar_musket',             onBack = true},
-    ['WEAPON_MICROSMG']           = {model = 'w_sb_microsmg',           onBack = true},
-    ['WEAPON_PUMPSHOTGUN_MK2']    = {model = 'w_sg_pumpshotgunmk2',     onBack = true},
-    ['WEAPON_PUMPSHOTGUN']        = {model = 'w_sg_pumpshotgun',        onBack = true},
-    ['WEAPON_SAWNOFFSHOTGUN']     = {model = 'w_sg_sawnoff',            onBack = true},
-    ['WEAPON_SMG_MK2']            = {model = 'w_sb_smgmk2',             onBack = true},
-    ['WEAPON_SMG']                = {model = 'w_sb_smg',                onBack = true},
-    ['WEAPON_SNIPERRIFLE']        = {model = 'w_sr_sniperrifle',        onBack = true},
-    ['WEAPON_SPECIALCARBINE_MK2'] = {model = 'w_ar_specialcarbinemk2',  onBack = true},
-    ['WEAPON_SPECIALCARBINE']     = {model = 'w_ar_specialcarbine',     onBack = true},
-    ['WEAPON_SWEEPERSHOTGUN']     = {model = 'w_sg_sweeper',            onBack = true},
-    ['WEAPON_COMBATMG']     	  = {model = 'w_mg_combatmg',     		onBack = true},
-    ['WEAPON_COMBATMG_MK2']       = {model = 'w_mg_combatmgmk2',        onBack = true}
+local weaponData = { -- Thanks to https://github.com/itzhapp/ShowWeapon
+	back = {
+		list = {
+			['WEAPON_ADVANCEDRIFLE']		= {model = 'w_ar_advancedrifle'		},
+			['WEAPON_ASSAULTRIFLE']			= {model = 'w_ar_assaultrifle'		},
+			['WEAPON_ASSAULTSHOTGUN']		= {model = 'w_sg_assaultshotgun'	},
+			['WEAPON_ASSAULTSMG']			= {model = 'w_sb_assaultsmg'		},
+			['WEAPON_BULLPUPRIFLE_MK2']		= {model = 'w_ar_bullpupriflemk2'	},
+			['WEAPON_BULLPUPRIFLE']			= {model = 'w_ar_bullpuprifle'		},
+			['WEAPON_BULLPUPSHOTGUN']		= {model = 'w_sg_bullpupshotgun'	},
+			['WEAPON_CARBINERIFLE_MK2']		= {model = 'w_ar_carbineriflemk2'	},
+			['WEAPON_CARBINERIFLE']			= {model = 'w_ar_carbinerifle'		},
+			['WEAPON_COMBATPDW'] 			= {model = 'w_sb_smg'			},
+			['WEAPON_COMBATSHOTGUN']		= {model = 'w_sg_pumpshotgun'		},
+			['WEAPON_COMPACTRIFLE']			= {model = 'w_ar_assaultrifle'		},
+			['WEAPON_DBSHOTGUN']			= {model = 'w_sg_sawnoff'		},
+			['WEAPON_FIREWORK']			= {model = 'w_lr_firework'		},
+			['WEAPON_GUSENBERG']			= {model = 'w_sb_gusenberg'		},
+			['WEAPON_HEAVYSHOTGUN']			= {model = 'w_sg_heavyshotgun'		},
+			['WEAPON_HEAVYSNIPER_MK2']		= {model = 'w_sr_heavysnipermk2'	},
+			['WEAPON_HEAVYSNIPER']			= {model = 'w_sr_heavysniper'		},
+			['WEAPON_MARKSMANRIFLE']		= {model = 'w_sr_marksmanrifle'		},
+			['WEAPON_MARKSMANRIFLE_MK2']		= {model = 'w_sr_marksmanriflemk2'	},
+			['WEAPON_MG']				= {model = 'w_mg_mg'			},
+			['WEAPON_MILITARYRIFLE']		= {model = 'w_sr_marksmanriflemk2'	},
+			['WEAPON_MUSKET']			= {model = 'w_ar_musket'		},
+			['WEAPON_MICROSMG'] 			= {model = 'w_sb_microsmg'		},
+			['WEAPON_PUMPSHOTGUN_MK2']		= {model = 'w_sg_pumpshotgunmk2'	},
+			['WEAPON_PUMPSHOTGUN']			= {model = 'w_sg_pumpshotgun'		},
+			['WEAPON_SAWNOFFSHOTGUN']		= {model = 'w_sg_sawnoff'		},
+			['WEAPON_SMG_MK2']			= {model = 'w_sb_smgmk2'		},
+			['WEAPON_SMG']				= {model = 'w_sb_smg'			},
+			['WEAPON_SNIPERRIFLE']			= {model = 'w_sr_sniperrifle'		},
+			['WEAPON_SPECIALCARBINE_MK2']		= {model = 'w_ar_specialcarbinemk2'	},
+			['WEAPON_SPECIALCARBINE']     		= {model = 'w_ar_specialcarbine'	},
+			['WEAPON_SWEEPERSHOTGUN']    		= {model = 'w_sg_sweeper'		},
+			['WEAPON_COMBATMG']     	 	= {model = 'w_mg_combatmg'		},
+			['WEAPON_COMBATMG_MK2']       		= {model = 'w_mg_combatmgmk2'		},
+		},
+		coords = {
+			pos = vector3(0.3, -0.15, 0.05),
+			rot = vector3(0.0, 0.0, 0.0)
+		}
+	},
+	front = {
+		list = {
+			['WEAPON_ASSAULTRIFLE_MK2']		= {model = 'w_ar_assaultriflemk2'	},
+		},
+		coords = {
+			pos = vector3(0.2, 0.20, -0.1),
+			rot = vector3(180.0, 220.0, 0.0)
+		}
+	},
+	left = {
+		list = {
+			['WEAPON_NIGHTSTICK']		= {model = 'w_me_nightstick'	},
+			['WEAPON_MACHETE']		= {model = 'prop_ld_w_me_machette'	},
+			-- ['WEAPON_CROWBAR']		= {model = 'w_me_crowbar'	},
+		},
+		coords = {
+			pos = vector3(0.0, 0.0, 0.225),
+			rot = vector3(0.0, -90.0, 90.0)
+		}
+	}
 }
 
-local weaponData2 =
-{
-    ['WEAPON_ASSAULTRIFLE_MK2']   = {model = 'w_ar_assaultriflemk2',    onBack = true}
+local weaponHolstered = {
+	['back'] = {name = nil, object = nil},
+	['front'] = {name = nil, object = nil},
+	['left'] = {name = nil, object = nil}
 }
-
-local weaponHolstered = {name = nil, object = nil}
-local weaponHolstered2 = {name = nil, object = nil}
 local weaponEquipped = {name = nil, object = nil}
 
-AddEventHandler('onResourceStop', function() -- playerDropped
-    -- DeleteObject(weaponholstered.object)
-
+AddEventHandler('playerDropped', function() -- onResourceStop
 	for k, v in pairs(GetGamePool('CObject')) do
-        if IsEntityAttachedToEntity(PlayerPedId(), v) then
-            SetEntityAsMissionEntity(v, true, true)
-            DeleteObject(v)
-            DeleteEntity(v)
-        end
+		if IsEntityAttachedToEntity(PlayerPedId(), v) then
+		    SetEntityAsMissionEntity(v, true, true)
+		    DeleteObject(v)
+		    DeleteEntity(v)
+		end
 	end
 end)
 
 AddEventHandler('ox_inventory:currentWeapon',function(currentWeapon)
-    local playerPed = PlayerPedId()
-    if not currentWeapon then 
-        if weaponEquipped and weaponEquipped.name then
-            if weaponData[weaponEquipped and weaponEquipped.name] then
-				local model = weaponData[weaponEquipped.name].model
+	local playerPed = PlayerPedId()
+	if not currentWeapon then 
+		if weaponEquipped and weaponEquipped.name then
+			for k in pairs(weaponData) do
+				if weaponData[k].list[weaponEquipped and weaponEquipped.name] then
+					local model = weaponData[k].list[weaponEquipped.name].model
 
-				while not HasModelLoaded(model) do
-					RequestModel(model)
-					Wait(0)
+					while not HasModelLoaded(model) do
+						RequestModel(model)
+						Wait(0)
+					end
+
+					if weaponHolstered[k].object then DeleteObject(weaponHolstered[k].object) end
+
+					weaponHolstered[k].name = weaponEquipped.name
+					weaponHolstered[k].object = CreateObject(model, 0, 0, 0, true, true, true)
+
+					AttachEntityToEntity(weaponHolstered[k].object, playerPed, GetPedBoneIndex(playerPed, 24816), weaponData[k].coords.pos, weaponData[k].coords.rot, true, true, false, false, 1, true)
+					
+					SetModelAsNoLongerNeeded(model)
 				end
-
-				if weaponHolstered.object then DeleteObject(weaponHolstered.object) end
-	
-                weaponHolstered.name = weaponEquipped.name
-                weaponHolstered.object = CreateObject(model, 0, 0, 0, true, true, true)
-
-
-                AttachEntityToEntity(weaponHolstered.object, playerPed, GetPedBoneIndex(playerPed, 24816), weaponData[weaponEquipped.name].onBack and 0.3 or 0.2, weaponData[weaponEquipped.name].onBack and -0.15 or 0.20, weaponData[weaponEquipped.name].onBack and 0.05 or -0.1, weaponData[weaponEquipped.name].onBack and 0.0 or 180.0, weaponData[weaponEquipped.name].onBack and 0.0 or 220.0, 0.0, true, true, false, false, 1, true)
-			elseif weaponData2[weaponEquipped and weaponEquipped.name] then
-				local model = weaponData2[weaponEquipped.name].model
-
-				while not HasModelLoaded(model) do
-					RequestModel(model)
-					Wait(0)
-				end
-				if weaponHolstered2.object then DeleteObject(weaponHolstered2.object) end
-	
-                weaponHolstered2.name = weaponEquipped.name
-                weaponHolstered2.object = CreateObject(model, 0, 0, 0, true, true, true)
-
-
-                AttachEntityToEntity(weaponHolstered2.object, playerPed, GetPedBoneIndex(playerPed, 24816), 0.2, 0.20, -0.1, 180.0, 220.0, 0.0, true, true, false, false, 1, true)
-
-			end
-
-			SetModelAsNoLongerNeeded(model)
-        end
-    else
-        weaponEquipped.name = currentWeapon.name
-		if currentWeapon then
-			if currentWeapon.name == weaponHolstered.name then
-				DeleteObject(weaponHolstered.object)
-				weaponHolstered.name = nil
-				weaponHolstered.object = nil
-			elseif currentWeapon.name == weaponHolstered2.name then
-				DeleteObject(weaponHolstered2.object)
-				weaponHolstered2.name = nil
-				weaponHolstered2.object = nil
 			end
 		end
-    end
+	else
+		weaponEquipped.name = currentWeapon.name
+		if currentWeapon then
+			for k in pairs(weaponData) do
+				if currentWeapon.name == weaponHolstered[k].name then
+					DeleteObject(weaponHolstered[k].object)
+					weaponHolstered[k].name = nil
+					weaponHolstered[k].object = nil
+				end
+			end
+		end
+	end
 end)
