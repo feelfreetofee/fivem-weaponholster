fdc8362c6f423d0ffafb78457443f07d84b514ff
feelfreetofee <89666307+feelfreetofee@users.noreply.github.com>
2022-07-12 10:22:59 +0200

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -4,6 +4,14 @@ game 'gta5'
 author 'VIRUXE'
 description 'Attaches weapons to the torso or back of a players Ped (using ox_inventory)'
 
-client_script {'client.lua', '@framework/imports.lua'}
+client_script {'client.lua', '@es_extended/imports.lua'}
+
+files {
+  'mp_m_freemode_01_mp_m_zurbanwar.meta',
+  'mp_f_freemode_01_mp_f_zurbanwar.meta'
+}
+
+data_file 'SHOP_PED_APPAREL_META_FILE' 'mp_m_freemode_01_mp_m_zurbanwar.meta'
+data_file 'SHOP_PED_APPAREL_META_FILE' 'mp_f_freemode_01_mp_f_zurbanwar.meta'
 
 dependency 'ox_inventory'
